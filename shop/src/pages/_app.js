d9e0cba0eb33f70547daa8d6d9fa877f7a9ba747
Vladikolev0321 <vladi.kolev2003@gmail.com>
2023-02-15 22:46:32 +0200

    feat: Add base next.js project and configure nextUi

---
+++ shop/src/pages/_app.js
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import Layout from '@/components/Layout'
+import { NextUIProvider } from '@nextui-org/react';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <NextUIProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </NextUIProvider>
+  )
+}
