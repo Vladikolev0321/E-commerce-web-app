d96c0c2bc37bed9791baf18913f0b12eb5cbb930
Vladikolev0321 <vladi.kolev2003@gmail.com>
2023-02-24 17:12:41 +0200

    feat: Add redux config and managing cart state

--- shop/src/pages/_app.js
+++ shop/src/pages/_app.js
@@ -2,15 +2,19 @@ import '@/styles/globals.css'
 import Layout from '@/components/Layout'
 import { NextUIProvider } from '@nextui-org/react';
 import { SessionProvider } from 'next-auth/react';
+import { Provider } from 'react-redux';
+import store from '../store/store';  
 
 export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
-      <SessionProvider session={pageProps.session}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
+      <Provider store={store}>
+        <SessionProvider session={pageProps.session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+      </Provider>
     </NextUIProvider>
   )
 }
