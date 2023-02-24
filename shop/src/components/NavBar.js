1acb9d5f5d80dcbdcc8aed6e1e8c9f295a225cf6
Vladikolev0321 <vladi.kolev2003@gmail.com>
2023-02-24 17:00:17 +0200

    feat: Add conditional render for navbar if user auth

--- shop/src/components/NavBar.js
+++ shop/src/components/NavBar.js
@@ -1,29 +1,44 @@
 import { Navbar, Button, Text } from "@nextui-org/react";
 import Link from "next/link";
-import {useRouter} from 'next/router'
+import { useRouter } from 'next/router'
+import { useSession, signOut } from "next-auth/react";
 
 const NavBar = () => {
+
+  // console.log("session", session);
+  const { data: session, status } = useSession();
   const router = useRouter();
   const isActive = (path) => {
     return router.pathname === path;
   };
   return (
     <Navbar isBordered variant="floating">
-        <Navbar.Content hideIn="xs" variant="highlight-rounded">
-          <Navbar.Link as={Link} isActive={isActive("/")} href="/">Features</Navbar.Link>
-          <Navbar.Link as={Link} isActive={isActive("/cart")} href="/cart">Cart</Navbar.Link>
-        </Navbar.Content>
-        <Navbar.Content>
-          <Navbar.Link as={Link} color="inherit" href="/signin">
-            Login
-          </Navbar.Link>
-          <Navbar.Item>
-            <Button auto flat as={Link} href="/register">
-              Sign Up
-            </Button>
-          </Navbar.Item>
-        </Navbar.Content>
-      </Navbar>
+      <Navbar.Content hideIn="xs" variant="highlight-rounded">
+        <Navbar.Link as={Link} isActive={isActive("/")} href="/">Features</Navbar.Link>
+        <Navbar.Link as={Link} isActive={isActive("/cart")} href="/cart">Cart</Navbar.Link>
+      </Navbar.Content>
+
+      <Navbar.Content>
+        {session ? (
+          <>
+            <Navbar.Link as={Link} href="/dashboard">Dashboard</Navbar.Link>
+            <Button auto flat onClick={signOut}>Logout</Button>
+          </>
+        ) : (
+          <>
+            <Navbar.Link as={Link} color="inherit" href="/signin">
+              Login
+            </Navbar.Link>
+            <Navbar.Item>
+              <Button auto flat as={Link} href="/register">
+                Sign Up
+              </Button>
+            </Navbar.Item>
+          </>
+        )}
+
+      </Navbar.Content>
+    </Navbar >
   )
 };
 
